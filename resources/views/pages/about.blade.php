0691d67dba56d353a3b7ba74acdeb5ba9009c439
CoulibalyKafaloAlex <kafalo.coulibaly@uvci.edu.ci>
2022-01-26 12:05:56 +0000

    section and layout create

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('content')
+    <p>Developed by 🏴‍☠ Zeno Corporation</p>
+    <a href="/"> Revenir à la page d'accueil</a>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('template')
+@section('content')
+    <h1>Hello From Dabou</h1>
+    <p>Il est actuellement {{date('h:i A')}} </p>
+
+@endsection
